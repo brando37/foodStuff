d6ce3ebbca78f29abc28e327f9015181d3751501
brando37 <mbsmith18@bellsouth.net>
2017-03-15 08:54:42 -0500

    mbs - account management added and tested

--- Food/Account.java
+++ Food/Account.java
@@ -1,21 +1,46 @@
 public class Account{
   	private double amount;
-  
-  	public Account(){
-   		amount = 0.0;
- 	}
-  
- 	public double getAmount(){
-    	return amount;
+    private String name;
+
+    public Account(String name){
+      this.name = name;
+      this.amount = 0.0;
+      this.printAmount();
+    }
+
+    public Account(String name,double initialAmt){
+      this.name = name;
+      this.amount = initialAmt;
+      this.printAmount();
+    }
+
+    public String getName(){
+      return this.name;
+    }
+
+ 	  public double getAmount(){
+    	return this.amount;
   	}
-  
-  	public void addAmount(double input){
-    	amount = amount + input;
+
+    public void printAmount(){
+      System.out.println(this.getName() + " contains " + this.getAmount());
+      return;
+    }
+
+  	private void addAmount(double input){
+    	this.amount = this.amount + input;
     	return;
   	}
-  
-  	public void subAmount(double output){
-  		amount = amount - output;
+
+  	private void subAmount(double output){
+  		this.amount = this.amount - output;
     	return;
   	}
+
+    public void transferTo(Account receiver,double amount){
+      System.out.println("Transferring " + amount + " from " + this.getName() + " to " + receiver.getName());
+      this.subAmount(amount);
+      receiver.addAmount(amount);
+      return;
+    }
 }
--- Food/Test.java
+++ Food/Test.java
@@ -1,6 +1,42 @@
+import java.io.*;
+
 public class Test{
 	public static void main(String[] args){
-    	Account savings = new Account();
-      	Account food = new Account();
+		String accountFile = "accounts.csv";
+		String line = "";
+		BufferedReader br = null;
+		String[][] accnts = new String[5][2];
+		int numAccnts = 0;
+		try{
+			br = new BufferedReader(new FileReader(accountFile));
+			while((line = br.readLine()) != null){
+				numAccnts ++;
+				accnts[numAccnts - 1] = line.split(",");
+			}
+		}
+		catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		finally{
+			if(br != null){
+				try{
+					br.close();
+				}
+				catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+
+    	Account Savings = new Account(accnts[0][0],Double.parseDouble(accnts[0][1]));
+      Account Food = new Account(accnts[1][0],Double.parseDouble(accnts[1][1]));
+
+			Food.transferTo(Savings,19.0);
+
+			Food.printAmount();
+			Savings.printAmount();
     }
 }
